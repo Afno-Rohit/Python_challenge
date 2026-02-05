c3cccb260a114c21c2eae0bb01723e044a1599c4
Rohit Shriwastav <internetworld1417@gmail.com>
2026-02-05 19:18:46 +0530

    This all code is basic code

--- ch_1_Basic.py
+++ ch_1_Basic.py
@@ -1,1 +1,11 @@
-print("Hello world")
+# print("Hello world")
+   
+# print("Name: Rohit")
+# print("Roll no:61")
+# print("class: BCA")
+
+# print("hello, i am Rohit from Nepal")
+# print("Nepal is the most beautiful place in the world")
+# print("Nepal is 2nd richest in water after brazil")
+ 
+# print("Hello, live life long life")

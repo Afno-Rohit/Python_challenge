663b797850df2d6afe53eb9e7da3e41340fa4acb
Rohit Shriwastav <internetworld1417@gmail.com>
2026-02-07 11:03:17 +0530

    intermediate of chapter 1 code is added

---
+++ ch_1_intermediate.py
@@ -0,0 +1,42 @@
+# print("Algebraic relation is the procedural language query") #this is from DBMS
+
+# print("There is a person sitting beside me") #include me for the person
+#                                             #write essay on it
+# import math
+# print("Value of pi:", math.pi)
+
+# import random 
+# print(random.randint(1,1000))
+
+# import datetime 
+# print(datetime.datetime.now())
+
+# import datetime
+
+# current_time = datetime.datetime.now()
+# five_minutes_ago = current_time - datetime.timedelta(minutes=5)
+
+# print("Current time:", current_time)
+# print("5 minutes ago:", five_minutes_ago)
+
+# import datetime
+
+# current_time = datetime.datetime.now()
+# five_days_ago = current_time - datetime.timedelta(days=5)
+
+# print("Current time:", current_time)
+# print("5 days ago:", five_days_ago)
+
+# import datetime
+
+# current_time = datetime.datetime.now()
+# five_years_ago = current_time - datetime.timedelta(days=5*365)
+
+# print("Current time:", current_time)
+# print("5 years ago:", five_years_ago)
+
+# import os
+# print(os.getcwd())
+
+import os
+print(os.listdir())

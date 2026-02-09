556d26ef5f158e65e4ff6b55d02157e6c291472d
Rohit Shriwastav <internetworld1417@gmail.com>
2026-02-09 21:26:59 +0530

    Adding chapter two basic code for practising

---
+++ ch_2_Basic.py
@@ -0,0 +1,30 @@
+# a="31.4"
+# b=float(a)
+# t=type(a)
+# print(t)
+
+# a=30
+# print(a)
+
+# a=4
+# b=7
+# print(a+b)
+
+# a=4
+# b=7
+# print(a-b)
+
+# a=4
+# b=7
+# print(a*b)
+
+a=8
+b=4
+print(a/b)
+
+
+
+
+# a=17
+# b=5
+# print(a%b)

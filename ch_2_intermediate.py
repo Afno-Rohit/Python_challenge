1c7e9e7ae5ee1d11738f65f55e1889d06f16f4c5
Rohit Shriwastav <internetworld1417@gmail.com>
2026-02-10 19:02:12 +0530

    Adding intermediate code of chapter 2

---
+++ ch_2_intermediate.py
@@ -0,0 +1,28 @@
+# a=input("Enter the value of a :")
+# print(type(a))
+
+# a="33"
+# b=int(a)
+# t=type(b)
+# print(t) 
+
+# a=33
+# b=float(a)
+# t=type(b)
+# print(t)
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b :"))
+# c=a>b
+# print("a is greater tham b is :",c)
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b :"))
+# average=(a+b)/2
+# print("The average of two number is :",average)
+
+# a=int(input("Enter the value of a:"))
+# print("the square of a number is :",a*a)
+
+a=(input("Enter the value of a :"))
+print(type(a))

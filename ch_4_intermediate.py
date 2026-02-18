55cec8b12706572a07f47c513752cbfa7d1a97a4
Rohit Shriwastav <internetworld1417@gmail.com>
2026-02-18 20:14:48 +0530

    Adding chapter 4 intermediate & advance code

---
+++ ch_4_intermediate.py
@@ -0,0 +1,22 @@
+# s=input("Enter a string :")
+# print("first character :",s[0])
+# print("last character :",s[-1])
+
+# name="Rohit"
+# print(name[1:5])
+
+# name = "Shriwastav"
+# print("Last character:", name[-1])
+# print("Last three characters:", name[-3:])
+
+# s="Shriwastav"
+# print(s[1:4:2])
+
+# name="Rohit"
+# print(name.endswith("it"))
+
+# character="Variable"
+# print(character.count("r"))
+
+character="rohit"
+print(character.capitalize())

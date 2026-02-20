b6e6872301266f2562e99dc7e6315aca15bfa4ef
Rohit Shriwastav <internetworld1417@gmail.com>
2026-02-20 20:30:01 +0530

    Adding chapter 4 intermediate code

---
+++ ch_4_intermediate.py
@@ -0,0 +1,38 @@
+# value=(10,20,30,40)
+# print(value[1:4])
+
+# value=[1,3,8,5,2]
+# value.sort()
+# print(value)
+
+# value=[1,3,8,5,2]
+# value.reverse()
+# print(value)
+
+# value=[1,3,8,5,2]
+# value.insert(1,2)
+# print(value)
+
+# value=[1,3,8,5,2]
+# value.remove(2)
+# print(value)
+
+# value=[1,3,8,5,2]
+# value.pop(3)
+# print(value)
+
+Marks=[]
+f1=input("Enter the marks of student :")
+Marks.append(f1)
+f2=input("Enter the marks of student :")
+Marks.append(f2)
+f3=input("Enter the marks of student :")
+Marks.append(f3)
+f4=input("Enter the marks of student :")
+Marks.append(f4)
+f5=input("Enter the marks of student :")
+Marks.append(f5)
+f6=input("Enter the marks of student :")
+Marks.append(f6)
+Marks.sort()
+print(Marks)

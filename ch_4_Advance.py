d647ecc72f34932b4058ebbd9230ccb0e5d00eb6
Rohit Shriwastav <internetworld1417@gmail.com>
2026-02-22 13:06:46 +0530

    Adding chapter 4 basic code

---
+++ ch_4_Advance.py
@@ -0,0 +1,13 @@
+# number=[1,3,4,5]
+# print(sum(number))
+
+# value=("Nepal","Bihar","ktm")
+# value[0]="Birgunj"
+# print(type(value))
+
+# value=(7,0,8,0,0,9)
+# print(value.count(0))
+
+# word=('a','b','c')
+# print(word.index('a'))
+
